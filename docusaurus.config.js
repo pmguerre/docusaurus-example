43e8bc8d14172750998458113cd848d59f9540be
Pedro Miguel Mendes Guerreiro <pmguerre@ualg.pt>
2025-04-15 15:50:33 +0100

    mudança do nome

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Exemplo do Docusarus',
+  tagline: 'Docs',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
